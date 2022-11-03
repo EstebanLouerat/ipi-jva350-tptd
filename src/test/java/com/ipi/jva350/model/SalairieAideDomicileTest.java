44a9a0b65ad5803a483b7102b09326581aa36773
steban <esteban.louerat@epitech.eu>
2022-11-03 17:08:51 +0100

    Add a test for salarie aide domile

---
+++ src/test/java/com/ipi/jva350/model/SalairieAideDomicileTest.java
@@ -0,0 +1,11 @@
+package com.ipi.jva350.model;
+
+import org.junit.jupiter.api.Test;
+
+public class SalairieAideDomicileTest {
+    
+    @Test
+    void aLegalementDroitADesCongesPayes() {
+
+    }
+}
