001aec4fbdd6fd7174ef8098e4eac8267f0570dd
steban <esteban.louerat@epitech.eu>
2022-11-04 11:25:36 +0100

    .

--- src/test/java/com/ipi/jva350/model/SalairieAideDomicileTest.java
+++ src/test/java/com/ipi/jva350/model/SalairieAideDomicileTest.java
@@ -19,15 +19,55 @@ public class SalairieAideDomicileTest {
     }
 
     @Test
-    void aLegalementDroitADesCongesPayesTrue() {
+    void aLegalementDroitADesCongesPayesNettementMoinsDe10() {
         // Given
+        LocalDate dummyDate = LocalDate.now();
         SalarieAideADomicile aide =  new SalarieAideADomicile("Jeanne", 
-        LocalDate.now(), LocalDate.now(), 0, 
-        0, 0, 0, 0);
+        dummyDate, LocalDate.now(), 0, 
+        0, 5, 0, 0);
         // When
         boolean res = aide.aLegalementDroitADesCongesPayes();
         // Then
         Assertions.assertEquals(false, res);
+
+    }
+    @Test
+    void aLegalementDroitADesCongesPayesNettementPlusDe10() {
+        // Given
+        LocalDate dummyDate = LocalDate.now();
+        SalarieAideADomicile aide =  new SalarieAideADomicile("Jeanne", 
+        dummyDate, LocalDate.now(), 0, 
+        0, 15, 0, 0);
+        // When
+        boolean res = aide.aLegalementDroitADesCongesPayes();
+        // Then
+        Assertions.assertEquals(true, res);
+
+    }
+    @Test
+    void aLegalementDroitADesCongesPayesLimmite10() {
+        // Given
+        LocalDate dummyDate = LocalDate.now();
+        SalarieAideADomicile aide =  new SalarieAideADomicile("Jeanne", 
+        dummyDate, LocalDate.now(), 0, 
+        0, 10, 0, 0);
+        // When
+        boolean res = aide.aLegalementDroitADesCongesPayes();
+        // Then
+        Assertions.assertEquals(true, res);
+
+    }
+    @Test
+    void aLegalementDroitADesCongesPayesLimmite11() {
+        // Given
+        LocalDate dummyDate = LocalDate.now();
+        SalarieAideADomicile aide =  new SalarieAideADomicile("Jeanne", 
+        dummyDate, LocalDate.now(), 0, 
+        0, 11, 0, 0);
+        // When
+        boolean res = aide.aLegalementDroitADesCongesPayes();
+        // Then
+        Assertions.assertEquals(true, res);
 
     }
 }
